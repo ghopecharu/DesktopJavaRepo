ee146fb9b24711d4d87cdf6c4afa5809840ba911
Charu <c.hire@gmail.com>
2021-05-16 17:49:21 +0000

    added test to branch1

---
+++ src/array1/Pg3.java
@@ -0,0 +1,22 @@
+package array1;
+
+public class Pg3 {
+
+	public static void main(String[] args) {
+		
+	int a[]=new int[5]; //Declaration and instantiation
+	//initialization.
+	
+	a[0]=10;
+	a[1]=20;
+	a[2]=30;
+	a[3]=40;
+	a[4]=50;
+	for (int i=0;i<a.length;i++)
+	{
+		System.out.println(a[i]);
+	}
+	
+	
+	}
+	}
